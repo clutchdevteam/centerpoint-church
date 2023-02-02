33f0d7bcaeeb65b6f2b52e6e64d780aae60cb3ce
Drew Clements <drewclementsdesign@gmail.com>
2023-02-01 22:57:07 -0500

    interior title

---
+++ components/blocks/InteriorTitle.vue
@@ -0,0 +1,64 @@
+<template>
+  <section
+    class="title-wrapper"
+    :style="`background: url(${block.backgroundImage.filename}) center center no-repeat; background-size: cover;`"
+  >
+    <div class="background-overlay" />
+    <div class="text-center z-10">
+      <BaseHeading class="text-white" size="h2" tag="h1">{{
+        block.title
+      }}</BaseHeading>
+      <p v-if="block.subtitle" class="text-accent text-lg">
+        {{ block.subtitle }}
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+import BaseHeading from "../base/BaseHeading.vue";
+
+export default {
+  components: {
+    BaseHeading,
+  },
+  props: {
+    block: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+.title-wrapper {
+  @apply relative overflow-hidden flex items-center justify-center;
+  min-height: 200px;
+}
+
+.background-overlay {
+  @apply bg-black inset-0 absolute opacity-90;
+  height: 100%;
+  z-index: 0;
+}
+
+.background-image {
+  position: absolute;
+  object-fit: cover;
+  height: 100%;
+  width: auto;
+  z-index: -3;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+}
+
+@media (min-width: 1270px) {
+  .background-image {
+    height: auto;
+    width: 100%;
+  }
+}
+</style>
--- plugins/components.js
+++ plugins/components.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import Page from "~/components/Page.vue";
+import InteriorPage from "~/components/InteriorPage.vue";
 import Hero from "~/components/blocks/Hero.vue";
 import TwoColumnImageRight from "~/components/blocks/TwoColumnImageRight.vue";
 import RichTextBlock from "~/components/blocks/RichTextBlock.vue";
+import InteriorTitle from "~/components/blocks/InteriorTitle";
 
 Vue.component("page", Page);
+Vue.component("InteriorPage", InteriorPage);
 Vue.component("Hero", Hero);
 Vue.component("TwoColumnImageRight", TwoColumnImageRight);
 Vue.component("RichTextBlock", RichTextBlock);
+Vue.component("InteriorTitle", InteriorTitle);
